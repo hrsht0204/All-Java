e156a3ec72a503fd869a3b073d7bce880e469c95
ARgod7 <krishanarpit2003@gmail.com>
2026-02-28 21:52:09 +0530

    Final Update

---
+++ Practise/test.java
@@ -0,0 +1,16 @@
+public class test
+{
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 7;
+        if(a > b)
+        {
+            if(b < a)
+            System.out.println((a==b)?Math.pow(5,2):Math.sqrt(64));
+            else
+            System.out.println(Math.round(7.5));
+            System.out.println("Control Statement");
+        }
+        
+    }
+}
